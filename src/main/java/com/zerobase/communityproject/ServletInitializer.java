5c3b0228f77214e0ebcb7eac2ebdd431d56adeab
CHOI <ceh201312@naver.com>
2023-02-28 11:56:29 +0900

    23.02.28 commit

---
+++ src/main/java/com/zerobase/communityproject/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.zerobase.communityproject;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CommunityprojectApplication.class);
+	}
+
+}
---
+++ src/test/java/com/zerobase/communityproject/CommunityprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.communityproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
