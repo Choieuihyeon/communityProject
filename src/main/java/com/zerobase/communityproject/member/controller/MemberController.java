d1f42626a7eca9eba61d2cbde49bfd815733c6bf
CHOI <ceh201312@naver.com>
2023-03-03 16:00:02 +0900

    회원가입/로그인 구현

---
+++ src/main/java/com/zerobase/communityproject/member/controller/MemberController.java
@@ -0,0 +1,40 @@
+package com.zerobase.communityproject.member.controller;
+
+import com.zerobase.communityproject.member.model.MemberInput;
+import com.zerobase.communityproject.member.service.MemberService;
+import javax.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class MemberController {
+
+	private final MemberService memberService;
+
+	@RequestMapping ("/member/login")
+	public String login() {
+
+		return "member/login";
+	}
+
+	@GetMapping ("/member/register")
+	public String register() {
+
+		return "member/register";
+	}
+
+	@PostMapping ("/member/register")
+	public String registerSubmit(Model model, HttpServletRequest request, MemberInput parameter) {
+
+		boolean result = memberService.register(parameter);
+		model.addAttribute("result", result);
+
+		return "member/register_complete";
+	}
+
+}
