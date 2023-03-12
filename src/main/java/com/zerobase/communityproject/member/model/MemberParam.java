80646b50d962c0dc75a5604df59f4d6874190b11
CHOI <ceh201312@naver.com>
2023-03-12 16:58:30 +0900

    회원 목록, 상세정보 구현

---
+++ src/main/java/com/zerobase/communityproject/member/model/MemberParam.java
@@ -0,0 +1,10 @@
+package com.zerobase.communityproject.member.model;
+
+import com.zerobase.communityproject.post.model.CommonParam;
+import lombok.Data;
+
+@Data
+public class MemberParam extends CommonParam {
+
+	String userId;
+}
